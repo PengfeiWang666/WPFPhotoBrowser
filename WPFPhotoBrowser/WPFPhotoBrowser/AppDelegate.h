383635789d9ee78ef24d7d21b7477d13c1019391
WangPengfei <wpf_silence@163.com>
2017-04-13 10:35:40 +0800

    项目初始化

---
+++ WPFPhotoBrowser/WPFPhotoBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WPFPhotoBrowser
+//
+//  Created by Leon on 2017/3/25.
+//  Copyright © 2017年 Leon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WPFPhotoBrowser/WPFPhotoBrowser/Demo/DemoMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  DemoMainViewController.h
+//  WPFPhotoBrowser
+//
+//  Created by Leon on 2017/3/25.
+//  Copyright © 2017年 Leon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DemoMainViewController : UIViewController
+
+@end
